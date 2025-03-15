35ff92b32845b96061ad0a6b6d81abe437e7704f
abhishek <abhishektk18@gmail.com>
2025-03-15 22:10:02 +0530

    new changes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import SidebarRow from "./SidebarRow";
 
-import { HashtagIcon, HomeModernIcon } from "@heroicons/react/24/outline";
-import { BellIcon } from "@heroicons/react/24/outline";
-import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import { BookmarkIcon } from "@heroicons/react/24/outline";
-import { CircleStackIcon } from "@heroicons/react/24/outline";
-import { EllipsisHorizontalCircleIcon } from "@heroicons/react/24/outline";
-import { UserIcon } from "@heroicons/react/24/outline";
+import {
+  HomeModernIcon,
+  HashtagIcon,
+  BellIcon,
+  EnvelopeIcon,
+  BookmarkIcon,
+  CircleStackIcon,
+  EllipsisHorizontalCircleIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 
 function Sidebar() {
   return (
@@ -17,14 +20,14 @@ function Sidebar() {
         src="/Twitter animated logo.png"
         alt=""
       />
-      <SidebarRow Icon={HomeModernIcon} title="Home" />
-      <SidebarRow Icon={HashtagIcon} title="Explore" />
-      <SidebarRow Icon={BellIcon} title="Notification" />
-      <SidebarRow Icon={EnvelopeIcon} title="Message" />
-      <SidebarRow Icon={BookmarkIcon} title="BookMarks" />
-      <SidebarRow Icon={CircleStackIcon} title="Lists" />
-      <SidebarRow Icon={UserIcon} title="Sign In" />
-      <SidebarRow Icon={EllipsisHorizontalCircleIcon} title="More" />
+      {/* <SidebarRow Icon={<HomeModernIcon />} title="Home" />
+      <SidebarRow Icon={<HashtagIcon />} title="Explore" />
+      <SidebarRow Icon={<BellIcon />} title="Notification" />
+      <SidebarRow Icon={<EnvelopeIcon />} title="Message" />
+      <SidebarRow Icon={<BookmarkIcon />} title="BookMarks" />
+      <SidebarRow Icon={<CircleStackIcon />} title="Lists" />
+      <SidebarRow Icon={<UserIcon />} title="Sign In" />
+      <SidebarRow Icon={<EllipsisHorizontalCircleIcon />} title="More" /> */}
     </div>
   );
 }
